eba334f3fd111e6ad8a14f242c377ec9cfef24e6
caiguojing <caiguojing@qianxin.com>
2022-09-14 14:49:16 +0800

    first code

---
+++ angular01/src/app/components/home/home.component.ts
@@ -0,0 +1,89 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+
+  public imgurl='https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png'
+
+  list = ['宝马','奥迪']
+
+  public today= new Date();
+
+  attr = 'wed'
+
+  
+  constructor() { 
+
+    console.log(this.today)
+
+  }
+
+  ngOnInit(): void {
+  }
+
+
+  run() {
+
+    alert(this.today)
+  }
+
+  setdata(){
+
+    this.attr = "mon"
+
+  }
+
+
+
+  keytest(val:any){
+    if (val.keyCode==13){
+    console.log("按了一下回车")}
+    else{
+
+      console.log(val.target)
+    }
+  }
+
+  
+  keywords='我是默认值'
+
+
+  
+  changewords(){
+
+
+    this.keywords='我是keywords改变后的值'
+
+  }
+
+
+  getwords(){
+
+
+    alert(this.keywords)
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+
+
+
---
+++ angular01/src/app/components/news/news.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-news',
+  templateUrl: './news.component.html',
+  styleUrls: ['./news.component.scss']
+})
+export class NewsComponent implements OnInit {
+
+
+  public title='我是新闻组件'
+
+  msg='我是一个新闻组件msg'
+
+  username:string='username'
+
+  public student:any='student'
+
+  public userinfo:any={
+
+    username:'zhang san',
+    age:'20'
+  }
+
+  public message:any;
+
+  public TT:any='另一个title'
+
+  public content='<h3>我是一个 html1 标签</h3>'
+
+
+  public arr=['1','3','5']
+
+  public userlist=[{
+
+      username:1,
+      age:10
+  },{
+
+    username:2,
+    age:20
+},{
+
+  username:3,
+  age:30
+}]
+
+public cars=[{
+
+  car:1,
+  list:[
+    {title:'x1',price:10},{title:'x2',price:20},{title:'x3',price:30}
+  ]
+},{
+
+  car:2,
+  list:[
+    {title:'q1',price:10},{title:'q2',price:20},{title:'q3',price:30}
+  ]
+}]
+
+
+  constructor() { 
+    this.message = 'everywhere'
+
+    console.log(this.msg)
+
+    this.msg = 'helloworld'
+
+  }
+
+  ngOnInit(): void {
+  }
+
+}
